970d30d0a7373b39bea7d3bce2fe4dd80a61364a
AnercioRibeiro <anercio.ribeiro@outlook.com>
2022-03-04 14:28:05 +0100

    First Commit

---
+++ ParkyAPI/ParkyMapping/ParkyMappings.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ParkyAPI.Models.DTOs;
+using ParkyAPI.Models;
+
+namespace ParkyAPI.ParkyMapping
+{
+    public class ParkyMappings : Profile
+    {
+        public ParkyMappings()
+        {
+            CreateMap<NationalPark,NationalParkDTO>().ReverseMap();
+        }
+    }
+}
